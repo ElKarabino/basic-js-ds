14fa7529ab958a960d2835cd7f801176204f3657
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2023-12-25 13:43:01 +0100

    feat: root,add,has,find binary

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,32 +1,67 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.rootNode = null
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.rootNode;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add (data) {
+    this.rootNode = addWithin(this.rootNode, data);
+    function addWithin(node, data) {
+      if (!node) {
+        return new Node (data)
+      }
+      if (data < node.data) {
+        node.left = addWithin(node.left, data)
+      } else if (data > node.data) {
+        node.rigth = addWithin(node.rigth, data)
+      }
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    return searchWithin(this.rootNode, data) !== null;
+    function searchWithin(node, data) {
+      if (!node) {
+        return null
+      }
+      if (data === node.data) {
+        return node;
+      } else if (data < node.data) {
+        return searchWithin(node.left, data)
+      } else if (data > node.data) {
+        return searchWithin(node.rigth, data)
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find (data) {
+    return searchWithin(this.rootNode, data);
+    function searchWithin(node, data) {
+      if (!node) {
+        return null
+      }
+      if (data === node.data) {
+        return node;
+      } else if (data < node.data) {
+        return searchWithin(node.left, data)
+      } else if (data > node.data) {
+        return searchWithin(node.rigth, data)
+      }
+    }
   }
+  
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
