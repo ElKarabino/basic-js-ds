7fa596c32b3f1b9450254ac4ec0fe7af736205f4
Stanislau Karbanovich <karbanovichstanislav@gmail.com>
2023-12-25 15:02:33 +0100

    feat: min max tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -98,13 +98,29 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return;
+    }
+
+    let node = this.rootNode;
+    while (node.left) {
+      node = node.left;
+    }
+
+    return node.data;
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.rootNode) {
+      return;
+    }
+
+    let node = this.rootNode;
+    while (node.right) {
+      node = node.right;
+    }
+
+    return node.data
   }
 }
 
